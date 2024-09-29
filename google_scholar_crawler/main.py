94801de13f1c42e51ee48888c31556739b33efab
huangyuxiang03 <huang-yx21@mails.tsinghua.edu.cn>
2024-09-29 22:38:51 +0800

    fiish

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -5,9 +5,9 @@ from datetime import datetime
 import os
 
 print("in main")
-
+print(f"scholar id: {os.environ['GOOGLE_SCHOLAR_ID']}")
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-print("get id")
+# print(f"get id: {}")
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
